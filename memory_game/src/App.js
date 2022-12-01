fd8059773bb03eef108964384071ae17c99724a4
kitem1tsUri <huslenbasketball@gmail.com>
2022-12-01 19:32:05 +0800

    react router added

---
+++ memory_game/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>easy</div>
+      <div>medium</div>
+      <div>hard</div>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ memory_game/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Easymode from "./Easy";
+import Mediummode from "./medium";
+import Hardmode from "./hard";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/easy" element={<Easymode />} />
+    <Route path="/medium" element={<Mediummode />} />
+    <Route path="/hard" element={<Hardmode />} />
+    </Routes>
+  </BrowserRouter>
+);
